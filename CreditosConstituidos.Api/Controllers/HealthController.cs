bb2c967ec4b50aa908edc39851933af8937249f2
Rafael-Armond <rafaelarmond054@gmail.com>
2025-11-22 19:24:53 -0300

    commit inicial

---
+++ CreditosConstituidos.Api/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CreditosConstituidos.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+
+        public HealthController ()
+        { }
+
+        [HttpGet("self")]
+        public IActionResult Self() => Ok(new {status = "Ok"});
+    }
+}
